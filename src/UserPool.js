5d5dcfee0f6525cc74850a2364af94c7ede3b180
fernandovissentini <fvissentini@gmail.com>
2022-11-16 17:34:00 +0100

    gitignore

--- src/UserPool.js
+++ src/UserPool.js
@@ -1,8 +1,8 @@
 import {CognitoUserPool} from 'amazon-cognito-identity-js';
 
 const poolData = {
-    UserPoolId: 'us-east-2_4rm7Wbp5o',
-    ClientId: '2gtqmi83nb9t0e7poh9ael83ei'
+    UserPoolId: '',
+    ClientId: ''
 };
 
 export default new CognitoUserPool(poolData);
